c4bd138a271f78bd2a0c14b612e012098aaa3513
Rakshit <rakshit.savsani@motadata.com>
2022-11-06 14:04:35 +0530

    add dto into project

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "Banking/err"
+import (
+	"Banking/dto"
+	"Banking/err"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -11,7 +14,41 @@ type Customer struct {
 	Status      string `db:"status"`
 }
 
+func (receiver Customer) StatusAsText() string {
+	status := "active"
+	if receiver.Status == "0" {
+		status = "inactive"
+	}
+	return status
+}
+
 type CustomerRepo interface {
 	FindAll(string) ([]Customer, *err.AppError)
 	ById(string) (*Customer, *err.AppError)
+}
+
+func (c Customer) ConvertToCustomerRest() (res *dto.CustomerRest) {
+	return &dto.CustomerRest{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateOfBirth: c.DateOfBirth,
+		Status:      c.StatusAsText(),
+	}
+}
+
+func ConvertToCustomerResArray(domainObj []Customer) (resObj []dto.CustomerRest) {
+	customerRes := make([]dto.CustomerRest, 0)
+	for _, customer := range domainObj {
+		customerRes = append(customerRes, dto.CustomerRest{
+			Id:          customer.Id,
+			Name:        customer.Name,
+			City:        customer.City,
+			Zipcode:     customer.Zipcode,
+			DateOfBirth: customer.DateOfBirth,
+			Status:      customer.StatusAsText(),
+		})
+	}
+	return customerRes
 }
--- service/customerService.go
+++ service/customerService.go
@@ -2,24 +2,40 @@ package service
 
 import (
 	"Banking/domain"
+	"Banking/dto"
 	"Banking/err"
 )
 
 type CustomerService interface {
-	GetAllCustomer(string) ([]domain.Customer, *err.AppError)
-	GetCustomerById(string) (*domain.Customer, *err.AppError)
+	GetAllCustomer(string) ([]dto.CustomerRest, *err.AppError)
+	GetCustomerById(string) (*dto.CustomerRest, *err.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepo
 }
 
-func (s DefaultCustomerService) GetAllCustomer(status string) ([]domain.Customer, *err.AppError) {
-	return s.repo.FindAll(status)
+func (s DefaultCustomerService) GetAllCustomer(status string) ([]dto.CustomerRest, *err.AppError) {
+	customerDom, error := s.repo.FindAll(status)
+
+	if error != nil {
+		return nil, error
+	}
+
+	customerRes := domain.ConvertToCustomerResArray(customerDom)
+
+	return customerRes, error
 }
 
-func (s DefaultCustomerService) GetCustomerById(id string) (*domain.Customer, *err.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomerById(id string) (*dto.CustomerRest, *err.AppError) {
+	customerDom, error := s.repo.ById(id)
+
+	if error != nil {
+		return nil, error
+	}
+
+	customerRes := customerDom.ConvertToCustomerRest()
+	return customerRes, error
 }
 
 func NewCustomerService(repo domain.CustomerRepo) DefaultCustomerService {
